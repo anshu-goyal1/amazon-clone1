ab898e5893e4a20f1d14549d75584484e41a2b96
Infinity <itsanshugoyal@gmail.com>
2021-10-23 18:37:17 +0530

    added carousel img function in homepage

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,27 @@
 import React from "react";
-import "./Home.css";
-import Product from "./Product";
 
+import Product from "./Product";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import "./Home.css";
 function Home() {
   return (
     <div className="home">
       <div className="home_container">
-        <img
-          className="home_image"
-          //src="https://m.media-amazon.com/images/I/61bRd1hd+CL._SX3000_.jpg"
-          //src="https://m.media-amazon.com/images/I/719mRKY7JsL._SX3000_.jpg"
-          src="https://m.media-amazon.com/images/I/71MwDPWV9XL._SX3000_.jpg"
-          alt=""
-        />
-
+        <div className="home_image">
+          <Carousel
+            autoPlay={true}
+            infiniteLoop={true}
+            showArrows={false}
+            showStatus={false}
+            dynamicHeight={true}
+            showThumbs={false}
+          >
+            <img src="https://m.media-amazon.com/images/I/71MwDPWV9XL._SX3000_.jpg" alt="" />
+            <img src="https://m.media-amazon.com/images/I/61bRd1hd+CL._SX3000_.jpg" alt="" />
+            <img src="https://m.media-amazon.com/images/I/719mRKY7JsL._SX3000_.jpg" alt="" />
+          </Carousel>
+        </div>
         <div className="home_row">
           <Product
             id="1"
